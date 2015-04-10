40168a03e3c21397c24aab9b6276204b2fe4e851
Taras Serdyuk <taras@techburg.io>
2015-04-10 15:58:26 +0300

    service internal structure

--- src/SSE/Service.hs
+++ src/SSE/Service.hs
@@ -2,15 +2,23 @@
 module SSE.Service where
 
 import Prelude
+import Control.Applicative ((<$>), (<*>))
+import Control.Concurrent.Chan (Chan)
+import Control.Concurrent.MVar (MVar, newMVar)
+import Data.Map.Strict (Map, empty)
 import Network.Socket (SockAddr)
 import Network.Wai (Application, Request)
 import Network.Wai.EventSource (ServerEvent)
 
 
-data SSErvice key = SSErvice
+data SSErvice key = SSErvice {
+	route :: Request -> key,
+	channels :: MVar (Map key (Chan ServerEvent, Int)),
+	keys :: MVar (Map SockAddr key)
+}
 
 sservice :: (Ord key) => (Request -> key) -> IO (SSErvice key)
-sservice = undefined
+sservice route = SSErvice route <$> newMVar empty <*> newMVar empty
 
 application :: SSErvice a -> Application
 application = undefined

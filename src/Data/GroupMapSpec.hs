335147293fe2bb922736b9c7694069045d30aaa2
Taras Serdyuk <taras@techburg.io>
2015-04-11 20:58:45 +0300

    one more group map test

--- src/Data/GroupMapSpec.hs
+++ src/Data/GroupMapSpec.hs
@@ -7,8 +7,13 @@ import Data.GroupMap as M
 
 
 spec :: Spec
+-- Use cascading tests
 spec = describe "GroupMap" $ do
 	it "should insert group and value for key" $ do
 		let gmap = insert 'a' 1 True empty
 		M.lookup 'a' gmap `shouldBe` Just (1, True)
 		M.lookupGroup 1 gmap `shouldBe` Just [('a', True)]
+	it "should add key to existing group" $ do
+		let gmap = insert 'a' 1 True $
+			insert 'b' 1 False empty
+		M.lookupGroup 1 gmap `shouldBe` Just [('a', True), ('b', False)]

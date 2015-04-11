901f157acb1914a9fcbebbbdca1361c0da098e66
Taras Serdyuk <taras@techburg.io>
2015-04-11 21:16:57 +0300

    group map dirty basic implementation with (Ord v)

--- src/Data/GroupMap.hs
+++ src/Data/GroupMap.hs
@@ -2,27 +2,28 @@
 module Data.GroupMap where
 
 import BasePrelude
-import Data.Map.Strict (Map)
-import Data.Set (Set)
+import Data.Map.Strict as M (Map, empty, insert, insertWith, lookup)
+import Data.Set as S (Set, singleton, toList, union)
 
 
 data GroupMap k g v = GM {
 	keys :: Map k (g, v),
-	groups :: Map g (Set k)
-}
+	groups :: Map g (Set (k, v))
+} deriving (Eq, Show)
 
 
 empty :: GroupMap k g v
-empty = undefined
+empty = GM M.empty M.empty
 
 lookup :: (Ord k) => k -> GroupMap k g v -> Maybe (g, v)
-lookup = undefined
+lookup k (GM ks _) = M.lookup k ks
 
 lookupGroup :: (Ord g) => g -> GroupMap k g v -> Maybe [(k, v)]
-lookupGroup = undefined
+lookupGroup g (GM _ gs) = fmap S.toList $ M.lookup g gs
 
-insert :: (Ord k, Ord g) => k -> g -> v -> GroupMap k g v -> GroupMap k g v
-insert = undefined
+insert :: (Ord k, Ord g, Ord v) => k -> g -> v -> GroupMap k g v -> GroupMap k g v
+insert k g v (GM ks gs) = GM (M.insert k (g, v) ks) gs' where
+	gs' = M.insertWith S.union g (S.singleton (k, v)) gs
 
 delete :: (Ord k) => k -> GroupMap k g v -> GroupMap k g v
 delete = undefined

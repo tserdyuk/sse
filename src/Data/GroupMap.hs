54ce842049f1e3a57dbf365dda597714025accfa
Taras Serdyuk <taras@techburg.io>
2015-04-11 21:28:39 +0300

    group map excessive (Ord v) removed

--- src/Data/GroupMap.hs
+++ src/Data/GroupMap.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NoImplicitPrelude #-} -- Move to test srunner
 
 module Data.GroupMap where
 
@@ -8,8 +9,16 @@ import Data.Set as S (Set, singleton, toList, union)
 
 data GroupMap k g v = GM {
 	keys :: Map k (g, v),
-	groups :: Map g (Set (k, v))
+	groups :: Map g (Set (KeyValue k v))
 } deriving (Eq, Show)
+
+newtype KeyValue k v = KV { kv :: (k, v) } deriving (Show)
+
+instance (Eq k) => Eq (KeyValue k v) where
+	(KV (k, _)) == (KV (k', _)) = k == k'
+
+instance (Ord k) => Ord (KeyValue k v) where
+	compare (KV (k, _)) (KV (k', _)) = compare k k'
 
 
 empty :: GroupMap k g v
@@ -19,11 +28,11 @@ lookup :: (Ord k) => k -> GroupMap k g v -> Maybe (g, v)
 lookup k (GM ks _) = M.lookup k ks
 
 lookupGroup :: (Ord g) => g -> GroupMap k g v -> Maybe [(k, v)]
-lookupGroup g (GM _ gs) = fmap S.toList $ M.lookup g gs
+lookupGroup g (GM _ gs) = fmap (map kv . S.toList) $ M.lookup g gs
 
-insert :: (Ord k, Ord g, Ord v) => k -> g -> v -> GroupMap k g v -> GroupMap k g v
+insert :: (Ord k, Ord g) => k -> g -> v -> GroupMap k g v -> GroupMap k g v
 insert k g v (GM ks gs) = GM (M.insert k (g, v) ks) gs' where
-	gs' = M.insertWith S.union g (S.singleton (k, v)) gs
+	gs' = M.insertWith S.union g (S.singleton $ KV (k, v)) gs
 
 delete :: (Ord k) => k -> GroupMap k g v -> GroupMap k g v
 delete = undefined

428c686b9ec83941d45684b24c5590caa963bd49
Taras Serdyuk <taras@techburg.io>
2015-04-12 14:32:50 +0300

    cascading spec third implementation, almost works

--- src/Test/Hspec/Cascading.hs
+++ src/Test/Hspec/Cascading.hs
@@ -2,32 +2,37 @@
 module Test.Hspec.Cascading (CascadingSpec, describe, it, shouldBe) where
 
 import BasePrelude hiding (cast)
-import Data.DList (DList, append, cons, singleton, snoc)
+import Data.DList (DList, append, cons, fromList, singleton, snoc)
 import qualified Data.DList as D (map)
 import Test.Hspec (Arg, Expectation, Spec, SpecWith)
 import qualified Test.Hspec as H (describe, it, shouldBe)
 
 
-type CascadingSpec = CascadingSpecM ()
+type CascadingSpec = CSpecExpr ()
 
-data CascadingSpecM a = CSpec {
-	getTitle :: String,
+data CSpecExpr a = CSpecExpr {
+	getTitle :: Maybe String,
 	getExpect :: IO (),
-	getSpecs :: DList (CascadingSpecM a)
-} | CExpectation (IO ())
+	getSpecs :: DList (CSpecExpr a)
+}
 
 
-instance Monad CascadingSpecM where
-	(>>) (CExpectation _) = error "Hell!"
-	(>>) (CSpec title expect specs) = \case
-		CSpec title' expect' specs' -> CSpec title expect (snoc (D.map cast specs) (CSpec title' expect' specs'))
-		CExpectation expect' -> CSpec title (expect >> expect') (D.map cast specs)
+instance Monad CSpecExpr where
+	-- Refactor with pattern synonyms
+	(CSpecExpr Nothing expect specs) >> (CSpecExpr Nothing expect' specs') =
+		CSpecExpr Nothing (expect >> expect') (append (D.map cast specs) specs')
+	(CSpecExpr Nothing expect specs) >> spec@(CSpecExpr (Just _) _ _) =
+		CSpecExpr Nothing expect (snoc (D.map cast specs) spec)
+	spec@(CSpecExpr (Just _) _ _) >> (CSpecExpr Nothing expect' specs') =
+		CSpecExpr Nothing expect' (cons (cast spec) specs')
+	spec@(CSpecExpr (Just _) _ _) >> spec'@(CSpecExpr (Just _) _ _) =
+		CSpecExpr Nothing (return ()) (fromList [cast spec, spec'])
+
 	(>>=) = error "Not Implemented"
 	return = error "Not Implemented"
 
-cast :: CascadingSpecM a -> CascadingSpecM b
-cast (CExpectation io) = CExpectation io
-cast (CSpec title expect specs) = CSpec title expect (D.map cast specs)
+cast :: CSpecExpr a -> CSpecExpr b
+cast (CSpecExpr title expect specs) = CSpecExpr title expect (D.map cast specs)
 
 
 describe :: String -> CascadingSpec -> Spec
@@ -35,12 +40,12 @@ describe subject spec = H.describe subject specs where
 	specs = sequence_ $ toList $ its spec
 
 its :: CascadingSpec -> DList (SpecWith (Arg Expectation))
-its (CSpec title expect specs) = cons (H.it title expect) its' where
+its (CSpecExpr (Just title) expect specs) = cons (H.it title expect) its' where
 	its' = foldr append empty $ D.map its specs
 
 
 it :: String -> CascadingSpec -> CascadingSpec
-it title spec = CSpec title (return ()) empty >> spec
+it title (CSpecExpr Nothing expect specs) = CSpecExpr (Just title) expect specs
 
 shouldBe :: (Eq a, Show a) => a -> a -> CascadingSpec
-shouldBe x y = CExpectation (H.shouldBe x y)
+shouldBe x y = CSpecExpr Nothing (H.shouldBe x y) empty

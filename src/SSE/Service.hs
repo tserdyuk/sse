295c72e6ef7c6057bbf16ad560d53d620f727a83
Taras Serdyuk <taras@techburg.io>
2015-04-12 18:05:21 +0300

    sse no receiver send support

--- src/SSE/Service.hs
+++ src/SSE/Service.hs
@@ -31,8 +31,11 @@ onClose :: (Ord key) => SSErvice key -> SockAddr -> IO ()
 onClose (SSErvice _ state) addr = modifyMVar_ state (return . M.delete addr)
 
 send :: (Ord key) => SSErvice key -> key -> ServerEvent -> IO ()
-send (SSErvice _ state) key se = readMVar state >>=
-	flip writeChan se . snd . head . fromJust . lookupGroup key
+send service key se = do
+	state <- readMVar (getState service)
+	case lookupGroup key state of
+		Just ((_, channel) : _) -> writeChan channel se
+		Nothing -> return ()
 
 
 subscribe :: (Ord key) => SockAddr -> key -> State key -> State key

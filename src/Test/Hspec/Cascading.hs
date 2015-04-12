8fe4b9bf8ef68ebe32c358e17f8924c169e00b11
Taras Serdyuk <taras@techburg.io>
2015-04-12 11:18:52 +0300

    cascading spec with single top-level it interface

--- src/Test/Hspec/Cascading.hs
+++ src/Test/Hspec/Cascading.hs
@@ -1,9 +1,9 @@
 
-module Test.Hspec.Cascading (CascadingSpec, describe, it, shouldBe, spec) where
+module Test.Hspec.Cascading (CascadingSpec, describe, it, shouldBe) where
 
 import BasePrelude hiding (cast, map)
 import Data.DList (DList, map, singleton, snoc)
-import qualified Test.Hspec as H
+import qualified Test.Hspec as H (Spec, it, shouldBe)
 
 
 type CascadingSpec = CascadingSpecM ()
@@ -23,12 +23,11 @@ cast (CExpectation io) = CExpectation io
 cast (CSpec title specs) = CSpec title (map cast specs)
 
 
-spec :: CascadingSpec -> H.Spec
-spec (CSpec title specs) = undefined
+describe :: String -> CascadingSpec -> H.Spec
+describe subject (CSpec title specs) = undefined
 
-describe, it :: String -> CascadingSpec -> CascadingSpec
+it :: String -> CascadingSpec -> CascadingSpec
 it title spec = CSpec title (singleton spec)
-describe = it
 
 shouldBe :: (Eq a, Show a) => a -> a -> CascadingSpec
 shouldBe x y = CExpectation (H.shouldBe x y)

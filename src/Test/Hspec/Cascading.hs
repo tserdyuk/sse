e966b8fe86b08bf0ba9171ae9b24e2bfa8cd9e87
Taras Serdyuk <taras@techburg.io>
2015-04-12 10:45:49 +0300

    cascading spec do syntax support

--- src/Test/Hspec/Cascading.hs
+++ src/Test/Hspec/Cascading.hs
@@ -1,17 +1,29 @@
 
 module Test.Hspec.Cascading where
 
-import BasePrelude
+import BasePrelude hiding (cast)
+import Data.DList as D (DList, map, snoc)
 import Test.Hspec as H
 
 
-data CascadingSpec =
+type CascadingSpec = CascadingSpecM ()
+
+data CascadingSpecM a =
 	CExpectation (IO ()) |
-	CSpec String CascadingSpec
+	CSpec String (DList (CascadingSpecM a))
+
+instance Monad CascadingSpecM where
+	(CSpec title specs) >> spec = CSpec title (snoc (D.map cast specs) spec)
+	(>>=) = error "Not Implemented"
+	return = error "Not Implemented"
+
+cast :: CascadingSpecM a -> CascadingSpecM b
+cast (CExpectation io) = CExpectation io
+cast (CSpec title specs) = CSpec title (D.map cast specs)
 
 
 it :: String -> CascadingSpec -> CascadingSpec
-it (CSpec name specs) = undefined
+it title spec = undefined
 
 shouldBe :: (Eq a, Show a) => a -> a -> CascadingSpec
 shouldBe x y = undefined
